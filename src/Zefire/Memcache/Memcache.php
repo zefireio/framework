f24f04e415dd2522e6a52ce8e85b993ffa5ea9de
Zefire <zefireframework@gmail.com>
2018-03-30 12:56:15 +0100

    Handling Memcached options and auth

--- src/Zefire/Memcache/Memcache.php
+++ src/Zefire/Memcache/Memcache.php
@@ -37,9 +37,81 @@ class Memcache implements Storable, Connectable
      */
     public function connect()
     {
-        $this->memcache = new \Memcached();
-        foreach (\App::config('memcache.hosts') as $host => $port) {
-            $this->memcache->addServer($host, $port);
+        $this->memcache = new \Memcached('memcached_pool');
+        $config = (\App::config('memcache'));
+        if (isset($config['options']) && is_array($config['options']) && !empty($config['options'])) {
+            foreach ($config['options'] as $key => $value) {
+                if ($key == 'binary_protocol') {
+                    if ($value === true) {
+                        $this->memcache->setOption(\Memcached::OPT_BINARY_PROTOCOL, TRUE);    
+                    } else {
+                        $this->memcache->setOption(\Memcached::OPT_BINARY_PROTOCOL, FALSE);
+                    }                    
+                }
+                if ($key == 'tcp_no_delay') {
+                    if ($value === true) {
+                        $this->memcache->setOption(\Memcached::OPT_TCP_NODELAY, TRUE);    
+                    } else {
+                        $this->memcache->setOption(\Memcached::OPT_TCP_NODELAY, FALSE);
+                    }
+                }
+                if ($key == 'no_block') {
+                    if ($value === true) {
+                        $this->memcache->setOption(\Memcached::OPT_NO_BLOCK, TRUE);    
+                    } else {
+                        $this->memcache->setOption(\Memcached::OPT_NO_BLOCK, FALSE);
+                    }
+                }
+                if ($key == 'connect_timeout') {
+                    $this->memcache->setOption(\Memcached::OPT_CONNECT_TIMEOUT, $value);
+                }
+                if ($key == 'poll_timeout') {
+                    $this->memcache->setOption(\Memcached::OPT_POLL_TIMEOUT, $value);
+                }
+                if ($key == 'recv_timeout') {
+                    $this->memcache->setOption(\Memcached::OPT_RECV_TIMEOUT, $value);
+                }
+                if ($key == 'send_timeout') {
+                    $this->memcache->setOption(\Memcached::OPT_SEND_TIMEOUT, $value);
+                }
+                if ($key == 'failover' && $value === true) {
+                    $this->memcache->setOption(\Memcached::OPT_DISTRIBUTION, \Memcached::DISTRIBUTION_CONSISTENT);
+                }
+                if ($key == 'libketama_compatible') {
+                    if ($value === true) {
+                        $this->memcache->setOption(\Memcached::OPT_LIBKETAMA_COMPATIBLE, TRUE);    
+                    } else {
+                        $this->memcache->setOption(\Memcached::OPT_LIBKETAMA_COMPATIBLE, FALSE);
+                    }
+                }
+                if ($key == 'retry_timeout') {
+                    $this->memcache->setOption(\Memcached::OPT_RETRY_TIMEOUT, $value);
+                }
+                if ($key == 'server_failure_limit') {
+                    $this->memcache->setOption(\Memcached::OPT_SERVER_FAILURE_LIMIT, $value);
+                }
+                if ($key == 'auto_eject_hosts') {
+                    if ($value === true) {
+                        $this->memcache->setOption(\Memcached::OPT_AUTO_EJECT_HOSTS, TRUE);    
+                    } else {
+                        $this->memcache->setOption(\Memcached::OPT_AUTO_EJECT_HOSTS, FALSE);
+                    }
+                }
+                if ($key == 'auth') {
+                    if (is_array($value)) {
+                        if (isset($value['username']) && $value['username'] != '' && isset($value['password']) && $value['password'] != '') {
+                            $this->memcache->setSaslAuthData($value['username'], $value['password']);
+                        }
+                    }
+                }                
+            }
+        }
+        if (!$this->memcache->getServerList()) {
+            $servers = explode(',', $config['servers']);
+            foreach ($servers as $server) {
+                $parts = explode(':', $server);
+                $this->memcache->addServer($parts[0], $parts[1]);
+            }
         }
     }
     /**
